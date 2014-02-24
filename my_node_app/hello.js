f63ca2a4bb6ae0b67aceaa01a88361037c3d20c1
terry.cho <bwcho75@gmail.com>
2014-02-24 22:07:10 +0900

    add mynodeapp # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # On branch master # Changes to be committed: #   (use "git reset HEAD <file>..." to unstage) # #	new file:   my_node_app/.idea/.name #	new file:   my_node_app/.idea/encodings.xml #	new file:   my_node_app/.idea/misc.xml #	new file:   my_node_app/.idea/modules.xml #	new file:   my_node_app/.idea/my_node_app.iml #	new file:   my_node_app/.idea/scopes/scope_settings.xml #	new file:   my_node_app/.idea/vcs.xml #	new file:   my_node_app/.idea/workspace.xml #	new file:   my_node_app/hello.js # # Changes not staged for commit: #   (use "git add/rm <file>..." to update what will be committed) #   (use "git checkout -- <file>..." to discard changes in working directory) # #	deleted:    read_file/read_file.js # # Untracked files: #   (use "git add <file>..." to include in what will be committed) # #	hello_nodejs/.idea/ #	util_module/ git test

--- my_node_app/hello.js
+++ my_node_app/hello.js
@@ -3,4 +3,5 @@ http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World\n');
 }).listen(1337, "127.0.0.1");
+// git push test
 console.log('Server running at http://127.0.0.1:1337/');
