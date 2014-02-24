77d5fedd42ab744b45750d61210d29a0e1394379
terry.cho <bwcho75@gmail.com>
2014-02-24 22:52:35 +0900

    add simple http request handler

--- simple_http_server/simple_http.js
+++ simple_http_server/simple_http.js
@@ -1,5 +1,8 @@
 var http = require('http');
-var server = http.createServer();
+var server = http.createServer(function(request,response){
+    response.writeHead(200,{'Content-Type':'text/html'});
+    response.end(('Hello'));
+});
 
 server.listen(5000,function(){
     console.log('Server started');
@@ -7,8 +10,9 @@ server.listen(5000,function(){
 
 server.on('connection',function(){
     console.log('Client connected');
+    server.close();
 });
 
 server.on('close',function(){
-    console.log('Client disconnected');
+    console.log('server disconnected');
 });
